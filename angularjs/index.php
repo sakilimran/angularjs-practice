0c315815d89ad3f8f207e5a9500a3dea025e7bde
sakilimran <sakilcse7@gmail.com>
2014-08-17 19:23:50 +0600

    Updated some code

--- angularjs/index.php
+++ angularjs/index.php
@@ -3,10 +3,15 @@
 <head>
     <title> :: AngularJS Practice :: </title>
 </head>
-<body>
+<body ng-init="employees =[{username: 'Mahedi Azad', location: 'Mohammadpur'}, {username: 'Sakil Imran', location: 'Shukrabad'}, {username: 'Sohel Rana', location: 'Green Road'}]">
     <div class="container">
         Name: <input type="text" ng-model="name" /> <br/>
         {{ name }}
+        <ul>
+            <li ng-repeat="employee in employees">
+                {{ employee.username }} - {{ employee.location }}
+            </li>
+        </ul>
     </div>
 <script src="js/angular.min.js"></script>
 </body>
