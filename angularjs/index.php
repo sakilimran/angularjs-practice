1041a4257e79d372f40f179e8cd2638f368d6081
sakilimran <sakilcse7@gmail.com>
2014-08-17 15:22:07 +0600

    First test file created

---
+++ angularjs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html ng-app>
+<head>
+    <title> :: AngularJS Practice :: </title>
+</head>
+<body>
+    <div class="container">
+        Name: <input type="text" ng-model="name" /> <br/>
+        {{ name }}
+    </div>
+<script src="js/angular.min.js"></script>
+</body>
+</html>
